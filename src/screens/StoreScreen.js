7efea8bfbb4b729a673a51c22d859d6c07f076c6
Manu Barral <manuelbarral02@gmail.com>
2021-01-26 12:24:06 -0300

    Dummy screens

---
+++ src/screens/StoreScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const StoreScreen = () => {
+  return (
+    <View>
+      <Text>StoreScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default StoreScreen;
